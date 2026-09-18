3ef69737470bbf35d765ccc5e5d3f7bd9a02704e
Osacr92 <113170156+Osacr92@users.noreply.github.com>
2026-09-18 10:14:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ if time_to_insert:
     st.success(f'Well done {name_on_order}! Your Smoothie is ordered!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
