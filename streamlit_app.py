6a30f096de19a61dee663e7d272764c242f843c4
Osacr92 <113170156+Osacr92@users.noreply.github.com>
2026-09-18 10:42:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,6 @@ name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your smoothie will be:',name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredient_list = st.multiselect(
 'Choose up to 5 ingredients:'
@@ -27,11 +26,13 @@ ingredient_list = st.multiselect(
 )
 
 if ingredient_list:
-    ingredients_string=''
+  ingredients_string=''
 
-    for fruit_chosen in ingredient_list:
-        ingredients_string += fruit_chosen + ' '
-    #st.write(ingredients_string)
+  for fruit_chosen in ingredient_list:
+    ingredients_string += fruit_chosen + ' '
+    smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
+    st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
@@ -45,5 +46,4 @@ if time_to_insert:
     st.success(f'Well done {name_on_order}! Your Smoothie is ordered!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-# st.text(smoothiefroot_response.json())
 st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
